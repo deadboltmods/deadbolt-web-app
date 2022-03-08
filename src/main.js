0ed9d223f61669e1ce1be5271f272282844fd176
ITAIC <ithinkandicode@gmail.com>
2022-03-08 01:28:15 +0000

    Add Google Analytics/gtag

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
+import VueGtag from "vue-gtag";
+import VueScrollactive from 'vue-scrollactive';
 import '@/modules/registerServiceWorker'
 //import meta from '@/modules/meta'
 
 Vue.config.productionTip = false
 
-import VueScrollactive from 'vue-scrollactive';
+Vue.use(VueGtag, {
+  config: { id: "G-RZ7LPTDQ97" }
+});
+
 Vue.use(VueScrollactive);
 
 new Vue({
